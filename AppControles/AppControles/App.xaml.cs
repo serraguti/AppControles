65cb4af40df7a5357d0deb1fa002f25e89a6ae36
serraguti <pacoserranox@gmail.com>
2022-05-19 10:20:29 +0200

    Add project files.

---
+++ AppControles/AppControles/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using AppControles.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppControles
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new PaginaEventos();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppControles/AppControles/Views/PaginaEventos.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppControles.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PaginaEventos : ContentPage
+    {
+        bool IsActiveWindow;
+
+        public PaginaEventos()
+        {
+            InitializeComponent();
+            //SIEMPRE REALIZAR LAS ACCIONES DESPUES DE ESTE METODO
+            this.botonAccion.Clicked += MostrarNombre;
+            this.slider1.ValueChanged += Slider1_ValueChanged;
+            this.stepper1.ValueChanged += Stepper1_ValueChanged;
+        }
+
+        private void Stepper1_ValueChanged(object sender
+            , ValueChangedEventArgs e)
+        {
+            this.labelResultado.Text =
+                "Valor Stepper: " + e.NewValue;
+        }
+
+        private void Slider1_ValueChanged(object sender
+            , ValueChangedEventArgs e)
+        {
+            this.labelResultado.Text =
+                "Valor del Slider: " + e.NewValue;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            this.IsActiveWindow = true;
+            //INICIAMOS EL TIMER DEL DISPOSITIVO
+            Device.StartTimer
+                (TimeSpan.FromSeconds(0.1), IniciarProcesoTimer);
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            this.IsActiveWindow = false;
+        }
+
+        //METODO PARA CAMBIAR LA BARRA DE PROGRESO EN UN TIEMPO
+        //DETERMINADO
+        bool IniciarProcesoTimer()
+        {
+            this.progressBar1.Progress += 0.01;
+            return this.IsActiveWindow || 
+                this.progressBar1.Progress == 1;
+        }
+
+        private void MostrarNombre(object sender, EventArgs e)
+        {
+            string nombre = this.cajaNombre.Text;
+            this.labelResultado.Text = "Su nombre es " + nombre;
+        }
+    }
+}
